015abb6cb8ea2fb6837b2be029c01bddbe39c9c6
davyce <71631291+davyce@users.noreply.github.com>
2021-08-12 01:07:50 -0700

    Add files via upload

---
+++ Rolling_of_dice2.py
@@ -0,0 +1,25 @@
+#This program the rolling of dice 2.0.
+import random
+
+#Constant for the minimum and the maximum number
+MIN = 1
+MAX = 6
+
+def main():
+    #Create a function to controll the loop
+    again = 'y'
+
+    #Simulate rolling of the dice.
+    while again == 'Y' or again == 'y':
+        print('Rolling the dice...')
+        print('Their value are:')
+        x = random.randint(MIN, MAX)
+        y = random.randint(MIN, MAX)
+        print(x)
+        print(y)
+
+        #Do another roll of dice?
+        again = input('Roll them again?: ')
+
+#Call the main function
+main()
